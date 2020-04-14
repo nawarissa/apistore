5cd919d5c3516e03d13f160708fa1283ddee346c
Mouhsen Ibrahim <mohsen47@hotmail.co.uk>
2020-04-14 15:30:29 +0300

    Add meals, sizes and types tables.

---
+++ database/migrations/2020_04_14_121314_create_meals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->float("price");
+            $table->foreignId("size_id")->constrained();
+            $table->foreignId("type_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
